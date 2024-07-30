964f0b8440980f6c67143f7f7fd5811908981031
rteez <rfmussa@gmail.com>
2024-07-30 19:02:30 +0200

    changed responsive breakpoints

--- lib/app/view/responsive_wrapper.dart
+++ lib/app/view/responsive_wrapper.dart
@@ -12,11 +12,10 @@ class ResponsiveBreakPointWrapper extends StatelessWidget {
     return ResponsiveBreakpoints.builder(
       child: ResponsiveViewWrapper(child: child),
       breakpoints: [
-        const Breakpoint(start: 0, end: 450, name: MOBILE),
-        const Breakpoint(start: 451, end: 800, name: TABLET),
-        const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-        //const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-        //const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        const Breakpoint(start: 0, end: 440, name: MOBILE),
+        const Breakpoint(start: 440, end: 850, name: TABLET),
+        const Breakpoint(start: 850, end: 1920, name: DESKTOP),
+        const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
       ],
     );
   }
@@ -29,39 +28,22 @@ class ResponsiveViewWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaxWidthBox(
-      maxWidth: 1920,
-      child: ResponsiveScaledBox(
-        width: ResponsiveValue<double?>(context,
-            conditionalValues: const [
-              Condition.equals(name: MOBILE, value: 450),
-              Condition.equals(name: TABLET, value: 800),
-              Condition.equals(name: DESKTOP, value: 1000),
-              //Condition.equals(name: '4K', value: 3840),
-            ]).value,
-        child:
-            BouncingScrollWrapper.builder(context, child, dragWithMouse: true),
+    return Container(
+      child: MaxWidthBox(
+        maxWidth: 3840,
+        background: Container(color: Colors.indigo),
+        child: ResponsiveScaledBox(
+          width: ResponsiveValue<double?>(context,
+              conditionalValues: const [
+                const Condition.equals(name: MOBILE, value: 420),
+                const Condition.equals(name: TABLET, value: 800),
+                const Condition.equals(name: DESKTOP, value: 1800),
+                const Condition.equals(name: '4K', value: 3840),
+              ]).value,
+          child:
+              BouncingScrollWrapper.builder(context, child, dragWithMouse: true),
+        ),
       ),
-    );
-  }
-}
-
-/// A convenience widget to wrap main blocks with:
-///  - ResponsiveContraints for max width.
-///  - A Center to allow constraints to work in a List.
-class BlockWrapper extends StatelessWidget {
-
-  const BlockWrapper(this.widget, {super.key});
-  final Widget widget;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ResponsiveConstraints(conditionalConstraints: const [
-        Condition.equals(name: MOBILE, value: BoxConstraints(maxWidth: 600)),
-        Condition.equals(name: TABLET, value: BoxConstraints(maxWidth: 700)),
-        Condition.largerThan(name: TABLET, value: BoxConstraints(maxWidth: 1280)),
-      ], child: widget),
     );
   }
 }
